c3e287f556e3ec8387cab425454989a0f8b5edbc
Isma Navarro <ismanapa@gmail.com>
2022-12-27 14:59:13 +0100

    feat(12-3): sync context and local storage on add repository widget

---
+++ 12-local-storage-and-context/3-sync-context-and-local-storage/src/sections/dashboard/repositoryWidget/AddRepositoryWidgetForm.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+import { ReactComponent as Add } from "../../../assets/svgs/add.svg";
+import { RepositoryWidgetRepository } from "../../../domain/RepositoryWidgetRepository";
+import styles from "./AddRepositoryWidgetForm.module.scss";
+import { useAddRepositoryWidget } from "./useAddRepositoryWidget";
+
+type FormEvent<T> = React.FormEvent<HTMLFormElement> & {
+	target: { elements: { [key in keyof T]: { value: T[key] } } };
+};
+
+type FormFields = { id: string; repositoryUrl: string };
+
+export function AddRepositoryWidgetForm({
+	repository,
+}: {
+	repository: RepositoryWidgetRepository;
+}) {
+	const [isFormActive, setIsFormActive] = useState(false);
+	const [hasAlreadyExistsError, setHasAlreadyExistsError] = useState(false);
+	const { save } = useAddRepositoryWidget(repository);
+
+	const submitForm = async (ev: FormEvent<FormFields>) => {
+		ev.preventDefault();
+		const { id, repositoryUrl } = ev.target.elements;
+		const error = await save({ id: id.value, repositoryUrl: repositoryUrl.value });
+		setHasAlreadyExistsError(!!error);
+		setIsFormActive(false);
+	};
+
+	return (
+		<article className={styles.add_widget}>
+			<div className={styles.container}>
+				{!isFormActive && !hasAlreadyExistsError ? (
+					<button onClick={() => setIsFormActive(true)} className={styles.add_button}>
+						<Add />
+						<p>Añadir repositorio</p>
+					</button>
+				) : (
+					<form className={styles.form} onSubmit={submitForm}>
+						<div>
+							<label htmlFor="id">Id</label>
+							<input type="text" name="id" id="id" />
+						</div>
+						<div>
+							<label htmlFor="repositoryUrl">Url del repositorio</label>
+							<input type="text" name="repositoryUrl" id="repositoryUrl" />
+						</div>
+
+						{hasAlreadyExistsError && (
+							<p role="alert" aria-describedby="duplicated-error">
+								<span id="duplicated-error">Repositorio duplicado</span>
+							</p>
+						)}
+
+						<div>
+							<input type="submit" value="Añadir" />
+						</div>
+					</form>
+				)}
+			</div>
+		</article>
+	);
+}
